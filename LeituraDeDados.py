d9c5f06f6d67d88d15343a08f33cf9600b113fd4
Fabio Venicius <fabiovenicius@yahoo.com.br>
2016-07-13 17:01:56 -0300

    Retirado 1 if desnecessario e acrescentado o valor

--- LeituraDeDados.py
+++ LeituraDeDados.py
@@ -1,30 +1,27 @@
 # -*- coding: utf-8 -*-
 import csv
 from csv import *
+
+from datetime import date
+
 import ContaLinhas
 from ContaLinhas import *
 # Programa para verificar linhas que deverão ser importadas
 
 
-extrato = open('c:\\dados\\extrato.csv', 'r')
+#Somente contagem de linhas do arquivo
+extrato = open('extrato.csv', 'r')
 totalLinhas = ContaLinhas.contaLinhas(extrato)
-extrato = open('c:\\dados\\extrato.csv', 'r')
-meuarquivo = open('c:\\dados\\LinhasExtrato.txt', 'w')
-ultimalinha = open('c:\\dados\\ultimoExtrato.txt', 'w')
+extrato.close()
+
+#Abertura do arquivo para extração de dados
+extrato = open('extrato.csv', 'r')
+meuarquivo = open('LinhasExtrato.txt', 'w')
 dados = csv.reader(extrato)
 qtdLinhas = 0
 for linhas in dados:
     qtdLinhas += 1
-#Iniciar após a linha de Saldo Anterior e percorrer até a penultima linha
-    if qtdLinhas == totalLinhas - 1:
-        data = linhas[0]
-        dependenciaOrigem = linhas[1]
-        historico = linhas[2]
-        dataDoBalancete = linhas[3]
-        numeroDoDocumento = linhas[4]
-        valor = linhas[5]
-        linha = data + ' ' + historico + '\n'
-        ultimalinha.write(linha)
+#Iniciar após a linha de Saldo Anterior e percorrer até a penultima linha, antes da linha com Saldo final
     if 2 < qtdLinhas < totalLinhas:
         data = linhas[0]
         dependenciaOrigem = linhas[1]
@@ -32,5 +29,5 @@ for linhas in dados:
         dataDoBalancete = linhas[3]
         numeroDoDocumento = linhas[4]
         valor = linhas[5]
-        linha = data + ' ' + historico + '\n'
+        linha = data + ' ' + historico + ' ' + valor + '\n'
         meuarquivo.write(linha)
