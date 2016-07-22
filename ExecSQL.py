2bf7d7d1334c19720c182378daff1d4f82166c25
Fabio Venicius <fabiovenicius@yahoo.com.br>
2016-07-22 16:29:33 -0300

    Função para listagem de clausulas select

--- ExecSQL.py
+++ ExecSQL.py
@@ -6,7 +6,7 @@ import ConectaDB
 import GravaLancamentos
 
 
-#Inserir Movimentação Financeira
+#Inserir, modificar, deletar
 def exec_comando(sql):
     db = ConectaDB.conn()
     cur = db.cursor()
@@ -14,6 +14,22 @@ def exec_comando(sql):
         cur.execute(sql)
         db.commit()
     except:
+        print 'Erro: Não foi possível Executar comando sql %s' %(sql)
         db.rollback()
     db.close
 
+def exec_select(sql, qtdcampos):
+    db = ConectaDB.conn()
+    cur = db.cursor()
+    cur.execute(sql)
+    valores = cur.fetchall()
+
+    try:
+        for linha in valores:
+            contador = 0
+            while(contador < qtdcampos):
+                print linha[contador]
+                contador += 1
+        db.close()
+    except:
+        print 'Erro ao processar comando: \n %s \n Quantidade de elementos informados: %s' %(sql,qtdcampos)
